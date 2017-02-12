1c816e1aae299f33a98694c897caccc363d382a1
gasanov <gasanov.sh@gmail.com>
2017-02-13 00:36:43 +0100

    dsd

--- application/controllers/Backend.php
+++ application/controllers/Backend.php
@@ -3,27 +3,39 @@
 
 	class Backend extends CI_Controller {
 
-	private $facebook;
+		private $facebook;
 
-	public function __construct() {
-		parent::__construct();
-		if(!$_SESSION['facebook-is-admin']){ redirect('/'); }
-		
-		$this->load->library('fblib');
-		$this->facebook = $this->fblib->getFacebook();
+		public function __construct() {
+			parent::__construct();
+			if(!$_SESSION['facebook-is-admin']){ redirect('/'); }
+			
+			$this->load->library('fblib');
+			$this->facebook = $this->fblib->getFacebook();
 
-		$this->load->model('UserService');
-		$this->load->model('ContestService');
-		$this->load->model('PhotoService');
-		$this->load->model('PrizeService');
-		$this->load->model('VoteService');
-	}
+			$this->load->model('UserService');
+			$this->load->model('PhotoService');
+			$this->load->model('PrizeService');
+			$this->load->model('VoteService');
+		}
 
-	public function index(){
-		$this->load->view('structure/admin_header.php');
-		$this->load->view('');
-		$this->load->view('structure/footer.php');
-	}
+		public function index(){
+			$this->load->view('structure/admin_header.php');
+			$this->load->view('structure/admin_index.php');
+			$this->load->view('structure/footer.php');
+		}
+
+		public function createContest(){
+			$this->load->model('ContestService');
+			if ($this->ContestService->getCurrentContest()!=null){
+				$_SESSION['facebook-is-admin'] = true;
+			}
+		}
+
+		public function getStats(){
+
+		}
+
+
 
 
 
---
+++ application/views/admin/admin_index.php
@@ -0,0 +1,20 @@
+<div class="container">
+	<!-- Logo & title -->
+	<div class="row lineback">
+		<div class="col-md-2 col-md-offset-2" id="logo">
+		</div>
+		<div class="col-md-6" id="titre">
+			<h1> Hello <?php echo $firstName; ?> </h1>
+			<h2> You are in administrator backoffice!</h2>
+			<h2> Be careful when making any changes.</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3" id="button_participate">
+			<a href="#" class="btn btn-primary btn-lg">Je participe !</a>
+		</div>
+		<div class="col-md-3" id="button_see">
+			<a href="/vote/index" class="btn btn-primary btn-lg">Votez !</a>
+		</div>
+	</div>
+</div>
