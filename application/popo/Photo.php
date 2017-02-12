0690db126acfa3f870bdcb1bae96ecdb569c477b
SergeAimeLesKiwis <leo.foltzrahem@gmail.com>
2017-02-12 14:24:53 +0100

    hjkjnhj

--- application/popo/Photo.php
+++ application/popo/Photo.php
@@ -15,6 +15,19 @@
 			$this->createdBy = $createdBy;
 		}
 
+		public function getLikeDiv() {
+			$div = '<div class="fb-like" ';
+			$div .= 'data-href="'.$this->getVoteUrl().'" ';
+			$div .= 'data-layout="box_count" ';
+			$div .= 'data-action="like" ';
+			$div .= 'data-size="small" ';
+			$div .= 'data-show-faces="false" ';
+			$div .= 'data-share="false">';
+			$div .= '</div>';
+
+			return $div;
+		}
+
 		public function getVoteUrl() {
 			return $this->facebookUrl.'/contest-'.$this->contest.'/photo-'.$this->photoId;
 		}
--- application/views/vote.php
+++ application/views/vote.php
@@ -3,19 +3,19 @@
 ?>
 
 <section>
-<?php
-	echo '<h1>Concours du '.$start.' au '.$end.'</h1>';
-	echo '<div class="row">';
-		
-	foreach ($photos as $photo) {
-		echo '<div class="col-md-12">';
-		echo 'Auteur : '.$photo->createdBy;
-		echo 'Vote : '.$photo->getVoteUrl();
-		echo '<img class="contest-picture" src="'.$photo->facebookUrl.'" alt="photo"/>';
+	<?php
+		echo '<h1>Concours du '.$start.' au '.$end.'</h1>';
+		echo '<div class="row">';
+			
+		foreach ($photos as $photo) {
+			echo '<div class="col-md-3 contest-item">';
+			echo '<img class="contest-picture" src="'.$photo->facebookUrl.'" alt="photo"/>';
+			echo 'Auteur : '.$photo->createdBy;
+			echo '<br />';
+			echo $photo->getLikeDiv();
+			echo '</div>';
+		}
+			
 		echo '</div>';
-		echo '------------------------------------------------';
-	}
-		
-	echo '</div>';
-?>
+	?>
 </section>
