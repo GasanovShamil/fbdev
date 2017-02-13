272a2a3d5a05ed0e31c987dd21f8ae2189d1ff73
gasanov <gasanov.sh@gmail.com>
2017-02-14 00:52:04 +0100

    fddfd

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['backend/createContest'] = 'backend/createContest';
-$route['backend/(:any)'] = 'backend/createContest/$1';
-$route['backend'] = 'backend';
+// $route['backend/createContest'] = 'backend/createContest';
+// $route['backend/(:any)'] = 'backend/createContest/$1';
+// $route['backend'] = 'backend';
 
 $route['default_controller'] = 'home';
 $route['(:any)'] = 'home/$1';
--- application/views/admin/create_contest.php
+++ application/views/admin/create_contest.php
@@ -2,22 +2,22 @@
 
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('/backend/createContest'); ?>
+<?php echo form_open('backend/createContest'); ?>
 	<div class="form-group">
 		<label for="name">Nom du concours:</label>
-		<input type="text" class="form-control" id="name">
+		<input type="text" class="form-control" id="name" name="name">
 	</div>
 	<div class="form-group">
 		<label for="startDate">Date de debut:</label>
-		<input type="date" class="form-control" id="startDate">
+		<input type="date" class="form-control" id="startDate" name="startDate">
 	</div>
 	<div class="form-group">
 		<label for="endDate">Date de fin:</label>
-		<input type="date" class="form-control" id="endDate">
+		<input type="date" class="form-control" id="endDate" name="endDate">
 	</div>
 	<div class="form-group">
 		<label for="prize">Prize:</label>
-		<input type="text" class="form-control" id="prize">
+		<input type="text" class="form-control" id="prize" name="prize">
 	</div>
 	
 	<button type="submit" class="btn btn-default">Create</button>
