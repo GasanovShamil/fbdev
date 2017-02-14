50ea7f0ee562be3a257956f891091a1c8fae9b1c
gasanov <gasanov.sh@gmail.com>
2017-02-14 23:43:09 +0100

    frf

--- application/views/structure/admin_header_search.php
+++ application/views/structure/admin_header_search.php
@@ -8,6 +8,7 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style/admin.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style/site.css" />
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/scripts/site.js"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/scripts/admin_nav.js"></script>
 	</head>
 
 	<body>
@@ -32,17 +33,43 @@
 				        </ul>
 				    </li>
 				</ul>
-				<form class="navbar-form navbar-left">
-				    <div class="form-group">
-					    <label for="before">Avant:</label>
-					    <input type="date" class="form-control" id="before">
-				    </div>
-				    <div class="form-group">
-				    	<label for="after">Apres:</label>
-				    	<input type="date" class="form-control" id="after">
-				    </div>
-				    <button type="submit" class="btn btn-default">Chercher</button>
-			    </form>
+				<!-- DROPDOWN LOGIN STARTS HERE  -->
+				<ul id="signInDropdown" class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <button type="button" id="dropdownMenu1" data-toggle="dropdown" class="btn btn-info navbar-btn dropdown-toggle"><i class="glyphicon glyphicon-user"></i> Search <span class="caret"></span></button>
+                        <ul class="dropdown-menu">
+                          <li style="width: 250px;">
+                                <form class="navbar-form form" role="form">
+                                    <div class="form-group">
+                                      <div class="input-group">
+                                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                            <label for="before">Before</label>
+                                            <input id="before" class="form-control" type="date" oninvalid="setCustomValidity('Please enter a valid email address!')" onchange="try{setCustomValidity('')}catch(e){}" required="">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock color-blue"></i></span>
+                                            <!--PASSWORD-->
+                                            <label for="after">After</label>
+                                            <input id="after" class="form-control" type="date" oninvalid="setCustomValidity('Please enter a password!')" onchange="try{setCustomValidity('')}catch(e){}" required="">
+                                        </div>
+                                    </div>
+                                    <!--  BASIC ERROR MESSAGE
+                                    <div class="form-group">
+                                    <label class="error-message color-red">*Email &amp; password don't match!</label>
+                                    </div>
+                                    -->
+                                    <div class="form-group">
+                                        <!--BUTTON-->
+                                        <button type="submit" class="btn btn-primary form-control">Login</button>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- DROPDOWN LOGIN ENDS HERE  -->
 				<ul class="nav navbar-nav navbar-right">
 					<li>
 						<?php echo anchor('/home/index', 'Site', 'title="Retour sur le site" class="glyphicon glyphicon-log-out"'); ?>
