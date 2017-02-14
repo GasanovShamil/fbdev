957a5462148e6b632c1645743d95b7c05cbb0e0f
SergeAimeLesKiwis <leo.foltzrahem@gmail.com>
2017-02-14 14:39:39 +0100

    fix backend

--- application/controllers/Backend.php
+++ application/controllers/Backend.php
@@ -13,27 +13,27 @@
 			$this->facebook = $this->fblib->getFacebook();
 		}
 
-		public function index(){
+		public function index() {
 			$this->load->view('structure/admin_header.php');
 			$this->load->view('admin/admin_index.php');
 			$this->load->view('structure/footer.php');
 		}
 
-		public function listContests(){
+		public function listContests() {
 
 			$this->load->view('structure/admin_header.php');
 
 			$this->load->view('structure/footer.php');
 		}
 
-		public function createContest(){
+		public function createContest() {
 			$this->load->model('ContestService');
 			$this->load->helper('form');
 			$this->load->library('form_validation');
 			
 			$data['title'] = 'Create contest';
 			
-			if ($this->ContestService->getCurrentContest() != null){
+			if ($this->ContestService->getCurrentContest() != null) {
 				$data['alert'] = 'Il y a un autre concours en cours. L\'ajout d\'un nouveau concours va désactiver l\'ancien !!!';
 			}
 			$this->form_validation->set_rules('name','Nom du concours', 'required');
@@ -41,21 +41,17 @@
 			$this->form_validation->set_rules('endDate', 'Date de fin', 'required');
 			$this->form_validation->set_rules('prize','Le pris', 'required');
 
-			if ($this->form_validation->run() === false)
-			{
+			if ($this->form_validation->run() === false) {
 				$this->load->view('structure/admin_header.php', $data);
 				$this->load->view('admin/create_contest.php');
 				$this->load->view('structure/footer.php');
-			}
-			else
-			{
-				$multiple = $this->input->post('multipleParticipation') != null ?1:0;
+			} else {
 				$this->ContestService->addContest(
 					$this->input->post('name'),
 					$this->input->post('startDate'),
 					$this->input->post('endDate'),
 					$this->input->post('prize'),
-					$multiple,
+					$this->input->post('multipleParticipation') != null,
 					$_SESSION['facebook-user-id']
 				);
 
@@ -65,21 +61,20 @@
 			}
 		}
 
-		public function getStats(){
+		public function getStats() {
 
 		}
 
 
-		public function verifDate($dateDebut) 
-		{
-			if ($dateDebut < date("Y-m-d"))
+		public function verifDate($start) {
+			if ($start < date("Y-m-d"))
 			{
-				$this->form_validation->set_message('verifDate', 'La date de début est dans le passé');
+				$this->form_validation->set_message('verifDate', 'La date de début est dans le passé !');
 				return false;
 			}
-			else if ($dateDebut > $this->input->post('endDate')) 
+			else if ($start > $this->input->post('endDate')) 
 			{
-				$this->form_validation->set_message('verifDate', 'La date de début est après la date de fin');
+				$this->form_validation->set_message('verifDate', 'La date de début est après la date de fin !');
 				return false;
 			}
 			//  else if () 
