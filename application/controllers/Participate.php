ae268b04779201109398b8adfa86c0961898279c
SergeAimeLesKiwis <leo.foltzrahem@gmail.com>
2017-02-14 16:20:18 +0100

    hjnj

--- application/controllers/Participate.php
+++ application/controllers/Participate.php
@@ -2,6 +2,7 @@
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
 	require_once(dirname(__FILE__).'/../libraries/appconfig.php');
+	require_once(dirname(__FILE__).'/../viewModels/Album.php');
 
 	class Participate extends CI_Controller {
 
