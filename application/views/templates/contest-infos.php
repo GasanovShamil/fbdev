4dd869e200c1b8a1e83917d86bc6666f78c94fe5
gasanov <gasanov.sh@gmail.com>
2017-02-14 22:21:37 +0100

    ffrfrf

--- application/views/templates/contest-infos.php
+++ application/views/templates/contest-infos.php
@@ -1,7 +1,10 @@
-<?php require_once(dirname(__FILE__).'/../../viewModels/Contest.php'); ?>
+<?php 
+require_once(dirname(__FILE__).'/../../viewModels/Contest.php'); 
+require_once('echoQuoted.php');
+?>
 
 <div class="panel panel-info">
-	<div class="panel-heading toggle-slide" data-slide="#contests">
+	<div class="panel-heading toggle-slide" data-slide=<?php quote('#contest-'.$contest->id); ?>>
 		<div class="row">
 			<div class="col-sm-11">
 				<h3 class="panel-title"><?php echo $contest->name; ?></h3>
@@ -13,7 +16,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="panel-body" id="contests">
+	<div class="panel-body" id=<?php quote('contest-'.$contest->id); ?>>
 		<div id="box-group" class="row">
 			<h3>Prix : <?php echo $contest->prize; ?></h3>
 			<h3><?php echo $contest->getDateRange(); ?></h3>
