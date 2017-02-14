4eda6121b317b46e4634f5ce2994e62ae8cfc6c7
SergeAimeLesKiwis <leo.foltzrahem@gmail.com>
2017-02-14 16:11:04 +0100

    lnjk

--- application/views/participate.php
+++ application/views/participate.php
@@ -1,19 +1,12 @@
 <div class="container">
 
-	<?php var_dump($check); ?>
-	<br /> ----------------------------------------------------------------------------------------
-
-
-
-
-
 	<?php $this->load->view('templates/contest-infos', array('contest' => $contest)); ?>
 
 	<div id="box-group" class="row">
 
 		<?php
-			foreach ($photos as $photo) { 
-				$this->load->view('templates/participate-box-image', array('photo' => $photo));
+			foreach ($albums as $album) { 
+				$this->load->view('templates/participate-box-image', array('album' => $album));
 			}
 		?>
 
--- application/views/templates/participate-box-image.php
+++ application/views/templates/participate-box-image.php
@@ -1,7 +1,16 @@
+<?php
+	require_once('echoQuoted.php');
+	require_once(dirname(__FILE__).'/../../viewModels/Album.php');
+?>
+
 <div class="col-sm-3 box">
 	<div class="row">
-		<div class="box-content center-div col-sm-12">
-			<img src=<?php quote($photo); ?> alt="Photo"/>
+		<div class="box-header col-sm-12">
+			<?php echo $album->name; ?>
+		</div>
+
+		<div class="box-content center-div col-sm-12" data-album=<?php quote($album->id); ?>>
+			<img src=<?php quote($album->cover); ?> alt="Album"/>
 		</div>
 	</div>
 </div>
