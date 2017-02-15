11ca881d972900e35bc54c424472003dfd9e4243
SergeAimeLesKiwis <leo.foltzrahem@gmail.com>
2017-02-15 03:52:40 +0100

    bhj

--- application/controllers/Participate.php
+++ application/controllers/Participate.php
@@ -127,7 +127,7 @@
 						'picture' => $photo
 					);
 
-					$response = $this->facebook->api('me/feed', 'POST', $data);
+					$response = $this->facebook->post('me/feed', $data);
 
 					redirect('/vote/index');
 				}
