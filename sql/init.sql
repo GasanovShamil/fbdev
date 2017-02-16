6b7ee5dab349a0110f33671cc9e7cd837aae9dc0
gasanov <gasanov.sh@gmail.com>
2017-02-16 11:05:46 +0100

    suppresion des photos des filles du script sql

--- sql/init.sql
+++ sql/init.sql
@@ -52,15 +52,7 @@ CREATE TABLE `Votes` (
 -- INSERT
 
 INSERT INTO `Users` (`facebookId`, `firstName`, `lastName`, `email`) VALUES 
-('admin', 'admin', 'admin', 'admin@mail.com'),
-('test-un', 'un', 'test', 'un@mail.com'),
-('test-deux', 'deux', 'test', 'deux@mail.com'),
-('test-trois', 'trois', 'test', 'trois@mail.com');
+('admin', 'admin', 'admin', 'admin@mail.com');
 
 INSERT INTO `Contests` (`name`,`startDate`, `endDate`, `prize`, `status`, `multipleParticipation`, `createdAt`, `createdBy`) VALUES 
 ('Concours Test', '2017-02-01', '2017-02-27', 'prix à gagner', 1, 1, '2017-02-10', 'admin');
-
-INSERT INTO `Photos` (`contest`, `facebookUrl`, `createdAt`, `createdBy`) VALUES 
-(1, 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/16602588_615885281936972_8509153474097716860_n.jpg?oh=6bc8067ac729309f45c3c9f513947ac0&oe=593CF5F6', '2017-02-10', 'test-un'),
-(1, 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/16641093_10211860358775207_3708443820249674489_n.jpg?oh=e9fd6ed37ee43aa57bb81f978fdb6b4d&oe=5939105B', '2017-02-10', 'test-deux'),
-(1, 'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-0/s480x480/16387300_995669103911333_2027950331764620754_n.jpg?oh=782da38efebbc509b0991d1ed7dbbb94&oe=594BEF56', '2017-02-10', 'test-trois');
